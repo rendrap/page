984603fe4745acf9adcd25126cf0cc64e6698a6e
rendrap <rendrapradana@gmail.com>
2018-05-23 20:51:06 +0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source "http://rubygems.org"
+
+gem "jekyll", "~> 3.7.3"
+
+group :jekyll_plugins do
+  gem "jekyll-sitemap"
+end
+#   gem "jekyll-paginate", "~> 1.1.0"
+
+gem "github-pages", "~>183", group: :jekyll_plugins
+# gem "github-pages", group: :jekyll_plugins
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "nokogiri", "~> 1.8.1"
+gem "wdm", "~> 0.1.1" if Gem.win_platform?
---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+// 'use strict';
+
+var path         = require('path');
+var gulp         = require('gulp');
+var less         = require('gulp-less');
+var minifycss    = require('gulp-minify-css');
+// Additional package for "Page" jekyll project
+var browserSync  = require('browser-sync');
+var cp           = require('child_process');
+var pug          = require('gulp-pug');
+var plumber      = require('gulp-plumber');
+var notify       = require('gulp-notify');
+const reload     = browserSync.reload;
+//CONFIG PATHS
+var config = {
+  pages  : './pages',
+  assets : './assets',
+  build:'./dist'
+};
+
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+
+
+//TASKS
+gulp.task('less', function () {
+  gulp.src(config.pages+'/less/pages.less')
+    .pipe(less({
+      paths: [config.pages+'/less/']
+    }))
+    .pipe(plumber({
+          errorHandler: notify.onError()
+      }))
+    .pipe(gulp.dest(config.pages+'/css/'))
+    .pipe(browserSync.reload({stream:true}));
+});
+
+// compile pug
+gulp.task('pug', function() {
+  return gulp.src("./_pug_includes/*.pug")
+      .pipe(plumber({
+          errorHandler: notify.onError()
+      }))
+      .pipe(pug({
+        pretty: true,
+        verbose: true
+      }))
+      .pipe(gulp.dest("./_includes"))
+      .pipe(browserSync.stream());
+});
+
+
+gulp.task('css-min', function(){
+  return gulp.src( [config.build+'./assets/css/*.css' , config.build+'./pages/css/*.css'])
+    .pipe(minifycss());
+});
+
+// Wait for jekyll-build, then launch the Server
+gulp.task('browser-sync', ['less', 'pug', 'jekyll-rebuild'], function() {
+    browserSync({
+        open: false,
+        logPrefix: 'Page Jekyll',
+        logFileChanges: true,
+        server: {
+            baseDir: '_site',
+        },
+    });
+});
+
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build', '--incremental'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+gulp.task('gh-pages', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build', '--config', '_config-ghpages.yml'], {stdio: 'inherit'})
+        .on('close', done);
+});
+
+// gulp.task('jekyll-build', () => {
+//     return require('child_process').exec('bundle exec jekyll build');
+// });
+
+
+// Rebuild Jekyll & do page reload
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+
+gulp.task('watch', function () {
+    gulp.watch(config.pages+'/less/*.less', ['less']);
+    gulp.watch(['./_pug_includes/*.pug'], ['pug']);
+    // gulp.watch('assets/js/*.js', ['js']);
+    gulp.watch(['*.md','*/*.md'], ['jekyll-rebuild']);
+    gulp.watch(['*.yml','_data/*.yml'], ['jekyll-rebuild']);
+    gulp.watch(['./*.html', '_layouts/*.html', '_includes/*.html', '_includes/*.md',
+    '_posts/*'], ['jekyll-rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------
+// var clean        = require('gulp-clean');
+// gulp.task('build',['less','copy'],function() {
+//  gulp.run('css-min');
+
+// });
+
+// gulp.task('clean', function(){
+// 	return gulp.src( config.build+'' , {read: false})
+// 		.pipe(clean());
+// });
+
+// gulp.task('copy', ['clean'],function () {
+// 	return gulp.src(['./**/*','!**/node_modules/**','!.gitgnore','!package.json','!Gruntfile.js','!gulpfile.js'])
+// 	.pipe(gulp.dest(config.build+''));
+// });
